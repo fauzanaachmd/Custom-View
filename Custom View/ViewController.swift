50396e894a9def4b6fff8a5185f86dbd3499f780
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2020-01-22 10:58:04 +0700

    Initial Commit

---
+++ Custom View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom View
+//
+//  Created by Fauzan Achmad on 22/01/20.
+//  Copyright © 2020 Fauzan Achmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
