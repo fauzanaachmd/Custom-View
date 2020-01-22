e360f3b1474f34e8723ed6d00cf03d58e7a3e952
Fauzan Achmad <fauzan.achmad@student.umn.ac.id>
2020-01-22 11:57:40 +0700

    create table view

--- Custom View/ViewController.swift
+++ Custom View/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    //    var meals = [Meal]()
+    
+    private var data: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in 1...100 {
+            data.append("\(i)")
+        }
+        
+        tableView.dataSource = self
     }
-
 
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        
+        let text = data[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+}
